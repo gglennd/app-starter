08a28b9a2c12be2d472e32c9c23aa38692ef8c77
gglennd <grdimaano18@gmail.com>
2025-07-25 11:03:08 +0800

    Added linter with antfu/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  type: "app",
+  formatters: true,
+  react: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  rules: {
+    "no-console": ["warn"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
