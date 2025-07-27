295740ff6a4be390177d78798a56f7f786979ca1
gglennd <grdimaano18@gmail.com>
2025-07-27 11:53:00 +0800

    Implement tanstack/react-start

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,50 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+/// <reference types="vite/client" />
+
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
 import * as React from "react";
 
+import appCss from "@/styles/app.css?url";
+
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1.0",
+      },
+      {
+        title: "gglennd's project",
+      },
+    ],
+    links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      { rel: "stylesheet", href: appCss },
+    ],
+  }),
   component: RootComponent,
 });
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
+    <RootDocument>
       <Outlet />
-    </React.Fragment>
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
   plugins: [
     tsconfigPaths(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
+    tanstackStart({ customViteReactPlugin: true, target: "node-server", tsr: { target: "react" } }),
     react(),
     tailwindcss(),
   ],
